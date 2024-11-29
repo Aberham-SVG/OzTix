f6d1b8bd3b6a8d27a8dce3cff2cebe9228f715a8
Aberham-SVG <abeofcu@gmail.com>
2024-11-28 22:02:32 -0600

    Update hamburger-menu.js

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
   const sidebar = document.getElementById('menu-sidebar');
-  const isOpen = sidebar.style.left === '0px';
-  sidebar.style.left = isOpen ? '-250px' : '0px';
+  sidebar.classList.toggle('active'); // Toggle the "active" class
 }
